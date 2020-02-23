57554995ef6e4b421473ea8fd0bbe0c3ed3d3018
Vakar Andrei <mr.vakar2016@gmail.com>
2020-02-23 13:35:51 +0300

    Await.result has been deleted

--- BikeData/src/main/scala/by/itechart/internship/Main.scala
+++ BikeData/src/main/scala/by/itechart/internship/Main.scala
@@ -4,23 +4,22 @@ import by.itechart.internship.config.{FlywayConfig, LightBendConfig}
 import by.itechart.internship.dao.DatabaseReader
 import by.itechart.internship.logic._
 
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
 
 object Main extends App {
   val configValues = LightBendConfig.configSetter
   val flywayConfig = FlywayConfig.initDatabaseStructure(configValues)
 
-  val result = FileReader.tableReader(configValues).map(_ => {
-    val dataTrips = DatabaseReader.writer().map{vector =>
+  val result = FileReader.tableReader(configValues).flatMap(_ => {
+    DatabaseReader.writer().map { vector =>
       val listOfRecords = List(
         GeneralStats.logicController(configValues, vector),
         UsageStats.logicController(configValues, vector),
         BikeStats.logicController(configValues, vector))
       FileWriter.tableWriter(configValues, listOfRecords)
     }
-    Await.result(dataTrips, 720.seconds)
   })
 
   Await.result(result, 720.seconds)
